7bc2b445b5c8f0e4a2ac1e0eef49921b84f786fc
Sébastien Muller <igniweb.net@gmail.com>
2015-02-26 00:25:02 +0100

    Update /home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,56 +1,14 @@
 <?php namespace App\Http\Controllers;
 
-use DB;
-use App\Hashtag;
-use App\Tweet;
+use App\Search;
 
 class HomeController extends Controller {
 
     public function index()
     {
-        $tweets = Tweet::all();
+        $search = Search::find(1);
 
-        $committed = $this->committed($tweets);
-
-        $hashtags = $this->hashtags();
-
-        return view('welcome', compact('tweets', 'committed', 'hashtags'));
-    }
-
-    private function committed($tweets, $count = 10)
-    {
-        $committed = [];
-
-        $pivots = [];
-        $indexed = [];
-        foreach ($tweets as $tweet)
-        {
-            $commitments = $tweet['retweets'] + $tweet['favorites'];
-
-            $pivots[$tweet['id']] = $commitments;
-            $indexed[$tweet['id']] = $tweet->toArray();
-        }
-
-        arsort($pivots);
-        $pivots = array_slice($pivots, 0, $count, true);
-
-        foreach ($pivots as $tweetId => $commitments)
-        {
-            $committed[] = $indexed[$tweetId] + ['commitments' => $commitments];
-        }
-
-        return $committed;
-    }
-
-    private function hashtags($count = 20)
-    {
-        return DB::table('hashtag_tweet')
-               ->select('hashtags.label', DB::raw('COUNT(`hashtag_tweet`.`hashtag_id`) AS `occurences`'))
-               ->join('hashtags', 'hashtag_tweet.hashtag_id', '=', 'hashtags.id')
-               ->groupBy('hashtag_tweet.hashtag_id')
-               ->orderBy('occurences', 'desc')
-               ->take($count)
-               ->get();
+        return view('welcome', compact('search'));
     }
 
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,25 +6,9 @@
 </head>
 <body>
     <div class="container">
-        <h1>#EnvoyeSpecial 2015-02-12 - 2015-02-14</h1>
-        <h2>Top tweets</h2>
-        <ul class="committed">
-            @foreach ($committed as $tweet)
-                <li title="{{ $tweet['user_account'] }}"><strong>{{ $tweet['commitments'] }}</strong> {{ $tweet['text'] }}</li>
-            @endforeach
-        </ul>
-        <h2>Hashtags</h2>
-        <ul class="hashtags">
-            @foreach ($hashtags as $hashtag)
-                <li><strong>{{ $hashtag->occurences }}</strong> #{{ $hashtag->label }}</li>
-            @endforeach
-        </ul>
-        <h2>Tweets <small>({{ count($tweets) }})</small></h2>
-        <ul class="tweets">
-            @foreach ($tweets as $tweet)
-                <li title="{{ $tweet->user_account }}">{{ $tweet->text }}</li>
-            @endforeach
-        </ul>
+        <h1>Twitter stats</h1>
+        <h2>Search {{ $search->q }} <small>{{ $search->from . ' &mdash; ' . $search->to }}</small></h2>
+        <pre><?php print_r(json_decode($search->stats, true)); ?></pre>
     </div>
 </body>
 </html>
